3f55796aa8c6a99a8ed4763f3638fa3f0060db65
RG-HC <al058785@ucam.mx>
2023-08-28 13:58:45 -0600

    Cambio de nombre en el MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:Taxi_Campeche_User/presentation/screens/login/login_screen.dart';
-import 'package:Taxi_Campeche_User/presentation/screens/splash_page/splash_page_screen.dart';
+import 'package:taxi_campeche_user/presentation/screens/login/login_screen.dart';
+import 'package:taxi_campeche_user/presentation/screens/splash_page/splash_page_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Taxi Campeche',
       debugShowCheckedModeBanner: false,
       home: SplashPageScreen(goToPage: LoginScreen(),)
     );
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,2 +1,2 @@
-export 'package:Taxi_Campeche_User/presentation/screens/splash_page/splash_page_screen.dart';
-export 'package:Taxi_Campeche_User/presentation/screens/login/login_screen.dart';
+export 'package:taxi_campeche_user/presentation/screens/splash_page/splash_page_screen.dart';
+export 'package:taxi_campeche_user/presentation/screens/login/login_screen.dart';
